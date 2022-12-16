dde4eb7eb75dcc97978ddca3f92583fbe0a6d5e5
katasu-git <katasu9923@gmail.com>
2022-12-16 22:12:56 +0900

    disp pikachu image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,20 @@
 import Head from "next/head";
+import Image from "next/image";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    const inner = async () => {
+      const response = await axios.get("https://pokeapi.co/api/v2/pokemon/25");
+      setPokemon(response.data);
+      console.log(response.data);
+    };
+    inner();
+  }, []);
+
   return (
     <>
       <Head>
@@ -9,7 +23,16 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="text-red-600">hello</main>
+      <main className="text-red-600">
+        {pokemon && (
+          <Image
+            src={pokemon.sprites.front_default}
+            width="200"
+            height="200"
+            alt={""}
+          />
+        )}
+      </main>
     </>
   );
 }
